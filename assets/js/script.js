cb4269c6903d4b65be05d521eba6b96b220c7f1a
Houssine <ekhay1@outlook.com>
2024-05-12 15:30:57 -0400

    Added a task windows and implement evenlistners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
+const float= $(".floatDiv");
+const addTaskBtn =$('#btn1');
+const storeTaskBtn =$('#btn2');
+
+// input selector
+let toDo;
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+ 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    
 
 }
 
@@ -35,4 +43,32 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+
+    addTaskBtn.click(function(){
+        $(".floatDiv").toggle();
+    });
+    
+    storeTaskBtn.click(function(e){
+
+        if(localStorage.getItem(toDo)=== null){
+            toDo=[];
+          }else{
+              toDo = JSON.parse(localStorage.getItem(toDo))
+          }
+        e.preventDefault();
+        $(".floatDiv").toggle();
+        console.log('You have click the button in floating Div');
+    
+        const task = {
+            title: $("#title").val(),
+            date: $("#date").val(),
+            task: $("#task").val().trim(),
+        }
+        toDo.push(task);
+        localStorage.setItem("toDo", JSON.stringify(toDo))
+    });
+     
+   
+   
 });
+
